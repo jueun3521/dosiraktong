a3bb8fedb33b90b82e7f21d160ede0be46c044e2
jueun3521 <jueun1004juju@gamil.com>
2023-07-14 16:26:47 +0900

    메인 이미지 슬라이드

--- js/script.js
+++ js/script.js
@@ -77,3 +77,25 @@
         }
     }
     });
+
+    // 비주얼 슬라이드
+    var swiper = new Swiper (".sw-visual" , {
+        effect: "fade",
+    // fadeEffect: {
+    //   crossFade: true,
+    // },
+    loop: true,
+    // 슬라이드의 모션 속도를 transition 맞춘다.
+    speed: 1500,
+    autoplay: {
+        delay: 2500,
+      // 사용자가 마우스 클릭 드래그로 이동하면
+      // 아래 구문이 없으면 autoplya 가 해제되므로
+      // 이것을 방지해 주기위한 처리
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".sw-visual-next",
+        prevEl: ".sw-visual-prev",
+    },
+    })
